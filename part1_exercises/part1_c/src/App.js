7a0302c09c9f17b934616213f27fcd2659ae7631
rafzul <rafi.zulfahmi@mail.ugm.ac.id>
2021-11-13 13:57:38 +0700

    refactoring -> event handling, breaking up into multiple child, passing state to child component

--- part1_exercises/part1_c/src/App.js
+++ part1_exercises/part1_c/src/App.js
@@ -1,16 +1,28 @@
 import React, { useState } from 'react'
 
 
+const Display =({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (<button onClick={onClick}>{text}</button>)
+
+
 const App = () => {
   const [ counter, setCounter ] = useState(0);
 
+  const increaseByOne = () => setCounter(counter +1);
+
+  const setToZero = ()  => setCounter(0)
+
+  const decreaseByOne = () => setCounter(counter -1);
+
   return (
     <>
-    <div>{counter}</div>
-    <button onClick={() => setCounter(counter + 1)}>plus</button>
-    <button onClick={() => setCounter(0)}>
-      reset
-      </button>
+    <Display counter={counter}/>
+    <Button text="plus" onClick={increaseByOne}>plus</Button>
+    <Button text="reset" onClick={setToZero}
+    >reset</Button>
+    <Button onClick={decreaseByOne} text="minus"
+    >minus</Button>
     </>
   )
 }
